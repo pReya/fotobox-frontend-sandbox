861352f4eebc6e14624346bd5c0f09a1802e9e6d
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-06-14 16:04:52 +0200

    feat: spinner in capture button

--- src/components/capture-button/capture-button.styl.d.ts
+++ src/components/capture-button/capture-button.styl.d.ts
@@ -1,4 +1,7 @@
 declare const styles: {
   readonly "outer": string;
+  readonly "clickable": string;
+  readonly "loader": string;
+  readonly "load5": string;
 };
 export = styles;
--- src/components/capture-button/capture-button.tsx
+++ src/components/capture-button/capture-button.tsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
 import styles from "./capture-button.styl";
 
 const CaptureButton = (): JSX.Element => {
+  const [isBusy, setIsBusy] = useState(false);
+
+  const takePicture = () => {
+    setIsBusy(true);
+    fetch("http://192.168.179.23:8080/picture")
+      .then((response) => {
+        console.log("Response: ", response);
+        setIsBusy(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setIsBusy(false);
+      });
+  };
+
   return (
     <span
-      onClick={() => {
-        console.log("Click");
-        fetch("http://192.168.179.23:8080/picture").then((response) =>
-          console.log("Response: ", response)
-        );
-      }}
-      className={styles.outer}
-    ></span>
+      onClick={takePicture}
+      className={clsx(styles.outer, !isBusy && styles.clickable)}
+    >
+      {isBusy && <div className={styles.loader}>Loading...</div>}
+    </span>
   );
 };
 
