a8f3e059df22763df1715d42877bcc738c958277
Moritz Stueckler <moritz.stueckler@gmail.com>
2020-06-14 15:38:38 +0200

    feat: use fallback url, when backend is not available

--- src/components/video-preview/video-preview.tsx
+++ src/components/video-preview/video-preview.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./video-preview.styl";
 
 const VideoPreview = (): JSX.Element => {
+  const [source, setSource] = useState(
+    "http://climatecam.gi.alaska.edu/mjpg/video.mjpg"
+  );
+
+  useEffect(() => {
+    const backendIp = "http://192.168.179.23:8080/";
+
+    fetch(backendIp).then((response) => {
+      if (response.status === 200) {
+        console.log("Found backend");
+        setSource(backendIp);
+      }
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
-      <img src="http://192.168.179.23:8080/" />
+      <img src={source} />
     </div>
   );
 };
